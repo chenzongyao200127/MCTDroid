f95c45e60422289ac3abe8ff5c26fc403114807c
chenzongyao200127 <2319251792@qq.com>
2025-04-03 12:56:56 +0800

    Enhance attack configurations and experiment execution; add support for multiple attack types and improve logging for attack results

--- adz.sh
+++ adz.sh
@@ -3,13 +3,18 @@
 
 # Common parameters
 PYTHON_CMD="python main.py"
-COMMON_PARAMS="-N 100 --attacker ADZ --ADZ_attack"
-P_VALUES=(10 30 50 100)
+P_VALUES=(10 20 30 40 50 100)
+
+# Attack configurations
+declare -A attack_params=(
+    ["ADZ"]="-N 100 --attacker ADZ --ADZ_attack"
+    ["RSA"]="-N 100 --attacker RSA --RSA_attack"
+)
 
 # Models and their classifiers
 declare -A model_classifier_map=(
     ["drebin"]="svm mlp"
-    # ["apigraph"]="svm"
+    ["apigraph"]="svm"
     ["mamadroid"]="rf 3nn"
     ["fd-vae"]="fd-vae-mlp"
 )
@@ -18,19 +23,23 @@ declare -A model_classifier_map=(
 run_experiments() {
     local model=$1
     local classifier=$2
+    local attack=$3
+    local params=$4
 
     for p in "${P_VALUES[@]}"; do
-        echo "Running experiment: $model-$classifier with P=$p"
-        $PYTHON_CMD -R "$model-$classifier-P$p" $COMMON_PARAMS \
+        echo "Running experiment: $model-$classifier with $attack and P=$p"
+        $PYTHON_CMD -R "$model-$classifier-$attack-P$p" $params \
             --detection "$model" \
             --classifier "$classifier" \
             -P "$p"
     done
 }
 
-# Run experiments for all models and their classifiers
-for model in "${!model_classifier_map[@]}"; do
-    for classifier in ${model_classifier_map[$model]}; do
-        run_experiments "$model" "$classifier"
+# Run experiments for all models, classifiers, and attacks
+for attack in "${!attack_params[@]}"; do
+    for model in "${!model_classifier_map[@]}"; do
+        for classifier in ${model_classifier_map[$model]}; do
+            run_experiments "$model" "$classifier" "$attack" "${attack_params[$attack]}"
+        done
     done
 done
